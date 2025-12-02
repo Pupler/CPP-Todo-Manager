1d02fa9462783ee2aa19057af674dec2141cbdff
Pupler <misha.reak45@gmail.com>
2025-12-02 19:42:23 +0100

    Feat: enhance UI with tasks numbering and help command

--- main.cpp
+++ main.cpp
@@ -15,10 +15,11 @@ struct Task {
 int main() {
     string input;
     vector<Task> tasks;
-    tasks.push_back(Task("Get a job", false));
+    tasks.push_back(Task("Buy a bread", false));
+    tasks.push_back(Task("Do homework", true));
     tasks.push_back(Task("Learn C#", false));
     cout << "\n====C++ TODO MANAGER====" << endl;
-    cout << "Commands: list, exit" << endl;
+    cout << "Commands: list, help, exit" << endl;
 
     while (true) {
         cout << "> ";
@@ -28,12 +29,17 @@ int main() {
             continue;
         } else if (input == "list") {
             for (int i = 0; i < tasks.size(); i++) {
-                cout << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
+                cout << i + 1 << ". " << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
             }
         } else if (input == "exit") {
             break;
+        } else if (input == "help") {
+            cout << "Available commands:\n";
+            cout << "  list - Show all tasks\n";
+            cout << "  help - Show this help\n";
+            cout << "  exit - Quit program\n";
         } else {
-            cout << "Invalid command!";
+            cout << "Invalid command!" << endl;
         }
 
         cout << endl;
