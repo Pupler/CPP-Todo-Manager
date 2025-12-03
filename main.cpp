90f77b05a9676902959754d93702dd5efbcf16ea
Pupler <misha.reak45@gmail.com>
2025-12-03 11:39:37 +0100

    Feat: implement clear command

--- main.cpp
+++ main.cpp
@@ -12,14 +12,19 @@ struct Task {
     }
 };
 
+void showHeader() {
+    cout << "\n====C++ TODO MANAGER====" << endl;
+    cout << "Commands: list, help, clear, exit" << endl;
+}
+
 int main() {
     string input;
     vector<Task> tasks;
     tasks.push_back(Task("Buy a bread", false));
     tasks.push_back(Task("Do homework", true));
     tasks.push_back(Task("Learn C#", false));
-    cout << "\n====C++ TODO MANAGER====" << endl;
-    cout << "Commands: list, help, exit" << endl;
+
+    showHeader();
 
     while (true) {
         cout << "> ";
@@ -31,13 +36,17 @@ int main() {
             for (int i = 0; i < tasks.size(); i++) {
                 cout << i + 1 << ". " << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
             }
-        } else if (input == "exit") {
-            break;
+        } else if (input == "clear") {
+            system("clear");
+            showHeader();
         } else if (input == "help") {
             cout << "Available commands:\n";
             cout << "  list - Show all tasks\n";
             cout << "  help - Show this help\n";
+            cout << "  clear - Clear console\n";
             cout << "  exit - Quit program\n";
+        } else if (input == "exit") {
+            break;
         } else {
             cout << "Invalid command!" << endl;
         }
