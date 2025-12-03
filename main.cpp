1fc3abe7a407cbd0dd0f00bef8ad38dabe4fcae1
Pupler <misha.reak45@gmail.com>
2025-12-03 12:01:38 +0100

    Feat: add command for creating new tasks + enhance UI with ANSI

--- main.cpp
+++ main.cpp
@@ -14,12 +14,23 @@ struct Task {
 
 void showHeader() {
     cout << "\n====C++ TODO MANAGER====" << endl;
-    cout << "Commands: list, help, clear, exit" << endl;
+    cout << "Commands: list, add, help, clear, exit" << endl;
+}
+
+void addTask(vector<Task>& tasks) {
+    string taskDesc;
+
+    cout << "Enter task description: ";
+    getline(cin, taskDesc);
+
+    tasks.push_back(Task(taskDesc, false));
+    cout << "Task added!" << endl;
 }
 
 int main() {
-    string input;
     vector<Task> tasks;
+
+    string input;
     tasks.push_back(Task("Buy a bread", false));
     tasks.push_back(Task("Do homework", true));
     tasks.push_back(Task("Learn C#", false));
@@ -34,14 +45,18 @@ int main() {
             continue;
         } else if (input == "list") {
             for (int i = 0; i < tasks.size(); i++) {
-                cout << i + 1 << ". " << tasks[i].description << " -- " << (tasks[i].is_completed ? "Yes" : "No") << endl;
+                cout << i + 1 << ". " << tasks[i].description << " " <<
+                (tasks[i].is_completed ? "[\033[32mDONE\033[0m]" : "[\033[31mNOT DONE\033[0m]") << endl;
             }
+        } else if (input == "add") {
+            addTask(tasks);
         } else if (input == "clear") {
             system("clear");
             showHeader();
         } else if (input == "help") {
             cout << "Available commands:\n";
             cout << "  list - Show all tasks\n";
+            cout << "  add - Add a new task\n";
             cout << "  help - Show this help\n";
             cout << "  clear - Clear console\n";
             cout << "  exit - Quit program\n";
